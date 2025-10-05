e5648c47d4a8217ec1c699a536ef1d0a7691ec7d
alex <kristin.fuller@my.tccd.edu>
2025-10-05 22:22:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+set -euo pipefail
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 1) 自动定位 feeds.conf.default
+FEEDS_FILE="feeds.conf.default"
+if [ ! -f "$FEEDS_FILE" ] && [ -f "openwrt/feeds.conf.default" ]; then
+  FEEDS_FILE="openwrt/feeds.conf.default"
+fi
 
+echo "[diy-part1] using: $FEEDS_FILE"
+
+# 2) 先删除可能已存在的同名源（保证幂等）
+sed -i '/^[[:space:]]*src-git[[:space:]]\+helloworld[[:space:]]/d' "$FEEDS_FILE"
+sed -i '/^[[:space:]]*#\?[[:space:]]*src-git[[:space:]]\+helloworld[[:space:]]/d' "$FEEDS_FILE"
+# 如果以后要加 passwall，也先删掉旧行（现在先不加）
+sed -i '/^[[:space:]]*src-git[[:space:]]\+passwall[[:space:]]/d' "$FEEDS_FILE"
+sed -i '/^[[:space:]]*#\?[[:space:]]*src-git[[:space:]]\+passwall[[:space:]]/d' "$FEEDS_FILE"
+
+# 3) 只追加一次你需要的源
+echo "src-git helloworld https://github.com/fw876/helloworld" >> "$FEEDS_FILE"
+# 如需 passwall，取消下一行注释
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> "$FEEDS_FILE"
+
+# 4) 打印确认
+echo "----- final $FEEDS_FILE -----"
+tail -n +1 "$FEEDS_FILE"
